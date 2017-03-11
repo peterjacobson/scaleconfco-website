c14e98bf2991f771ff5fb515ad9df14e01446a1a
pete <peterjacobson.nz@gmail.com>
2017-03-11 16:19:39 +1300

    add photo as background image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,23 +2,10 @@ import React, { Component } from 'react'
 import Nav from "../Nav"
 import SocialIcons from '../SocialIcons'
 
-let particles
-if (typeof window !== 'undefined') {
-  particles = require('particles.js')
-}
-
 export default class Hero extends Component {
-
-  componentDidMount() {
-    particles && particles.particlesJS.load('particles-js', '/assets/particles.json', function() {
-      // eslint-disable-next-line no-console
-      console.log('callback - particles.js config loaded');
-    })
-  }
-
   render() {
     return (
-      <div id="particles-js" className="w-100 gradient h-650">
+      <div id="header-wrapper" className="w-100 i-header h-650">
         <Nav />
         <div className="mw80 w-100 center relative">
           <header className="w-100 tc tl-l w-auto-l">
